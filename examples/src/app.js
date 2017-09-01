39b00f3e325cd6072f4bc89a2b99e9e050c6195f
Stephan Meijer <stephan@meijer.ws>
2017-09-01 14:00:58 +0200

    Use unsplash to remove some state

--- examples/src/app.js
+++ examples/src/app.js
@@ -3,118 +3,75 @@ import ReactDOM from 'react-dom';
 import Gallery from 'react-photo-gallery';
 import { withContentRect } from 'react-measure';
 import Lightbox from 'react-images';
-import jsonp from 'jsonp';
 
 import CustomImage from './CustomImage';
 
-function debounce(func, wait, immediate) {
-  let timeout;
-  return function() {
-    const context = this,
-      args = arguments;
-    let later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    const callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
+const randomBetween = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+const unsplash = 'https://source.unsplash.com/random';
+
+const noImg = window.location.search.indexOf('no-img=1') > -1;
+const photos = Array.from(Array(25)).map((x, idx) => {
+  const width = randomBetween(400, 1200);
+  const height = randomBetween(400, 1200);
+  const src = `${unsplash}/${width}x${height}`;
+
+  return {
+    src: noImg ? `about:blank?${idx}` : src,
+    width,
+    height,
+    title: `Photo ${idx}`,
+    alt: `Photo ${idx}`,
+    srcSet: noImg
+      ? []
+      : [
+          `${unsplash}/${Math.floor(width * 1.25)}x${Math.floor(height * 1.25)} ${Math.floor(width * 1.25)}w`,
+          `${unsplash}/${Math.floor(width * 1.5)}x${Math.floor(height * 1.5)} ${Math.floor(width * 1.5)}w`,
+          `${unsplash}/${Math.floor(width * 1.75)}x${Math.floor(height * 1.75)} ${Math.floor(width * 1.75)}w`,
+          `${unsplash}/${Math.floor(width * 2)}x${Math.floor(height * 2)} ${Math.floor(width * 2)}w`,
+        ],
+    sizes: ['(min-width: 480px) 50vw', '(min-width: 1024px) 33.3vw', '100vw'],
   };
-}
+});
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { photos: null, pageNum: 1, totalPages: 1, loadedAll: false, currentImage: 0 };
-    this.handleScroll = this.handleScroll.bind(this);
-    this.loadMorePhotos = this.loadMorePhotos.bind(this);
+
+    this.state = {
+      currentImage: null,
+    };
+
     this.closeLightbox = this.closeLightbox.bind(this);
     this.openLightbox = this.openLightbox.bind(this);
     this.gotoNext = this.gotoNext.bind(this);
     this.gotoPrevious = this.gotoPrevious.bind(this);
   }
-  componentDidMount() {
-    this.loadMorePhotos();
-    this.loadMorePhotos = debounce(this.loadMorePhotos, 200);
-    window.addEventListener('scroll', this.handleScroll);
-  }
-  handleScroll() {
-    let scrollY = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
-    if (window.innerHeight + scrollY >= document.body.offsetHeight - 50) {
-      this.loadMorePhotos();
-    }
-  }
-  loadMorePhotos(e) {
-    if (e) {
-      e.preventDefault();
-    }
-    if (this.state.pageNum > this.state.totalPages) {
-      this.setState({ loadedAll: true });
-      return;
-    }
 
-    const urlParams = {
-      api_key: '372ef3a005d9b9df062b8240c326254d',
-      photoset_id: '72157680705961676',
-      user_id: '57933175@N08',
-      format: 'json',
-      per_page: '21',
-      page: this.state.pageNum,
-      extras: 'url_m,url_c,url_l,url_h,url_o',
-    };
-
-    let url = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos';
-    url = Object.keys(urlParams).reduce((acc, item) => {
-      return acc + '&' + item + '=' + urlParams[item];
-    }, url);
-
-    jsonp(url, { name: 'jsonFlickrApi' }, (err, data) => {
-      let photos = data.photoset.photo.map(item => {
-        let aspectRatio = parseFloat(item.width_o / item.height_o);
-        return {
-          src: aspectRatio >= 3 ? item.url_c : item.url_m,
-          width: parseInt(item.width_o),
-          height: parseInt(item.height_o),
-          title: item.title,
-          alt: item.title,
-          srcSet: [
-            `${item.url_m} ${item.width_m}w`,
-            `${item.url_c} ${item.width_c}w`,
-            `${item.url_l} ${item.width_l}w`,
-            `${item.url_h} ${item.width_h}w`,
-          ],
-          sizes: ['(min-width: 480px) 50vw', '(min-width: 1024px) 33.3vw', '100vw'],
-        };
-      });
-      this.setState({
-        photos: this.state.photos ? this.state.photos.concat(photos) : photos,
-        pageNum: this.state.pageNum + 1,
-        totalPages: data.photoset.pages,
-      });
-    });
-  }
   openLightbox(event, { index }) {
     this.setState({
       currentImage: index,
       lightboxIsOpen: true,
     });
   }
+
   closeLightbox() {
     this.setState({
       currentImage: 0,
       lightboxIsOpen: false,
     });
   }
+
   gotoPrevious() {
     this.setState({
       currentImage: this.state.currentImage - 1,
     });
   }
+
   gotoNext() {
     if (this.state.photos.length - 2 === this.state.currentImage) {
       this.loadMorePhotos();
     }
+
     this.setState({
       currentImage: this.state.currentImage + 1,
     });
@@ -139,49 +96,36 @@ class App extends React.Component {
   }
 
   render() {
-    const { measureRef } = this.props;
+    const { measureRef, photos } = this.props;
+    const { currentImage, lightboxIsOpen } = this.state;
 
-    if (this.state.photos) {
-      return (
-        <div className="App" ref={measureRef}>
+    return (
+      <div className="App" ref={measureRef}>
+        <div style={{ margin: '0 auto', padding: '0 130px', minWidth: 400, maxWidth: '80%' }}>
           <Gallery
-            photos={this.state.photos}
+            photos={photos}
             columns={this.getColumnCount()}
             onClick={this.openLightbox}
-            //  ImageComponent={CustomImage}
+           // ImageComponent={CustomImage}
           />
-
-          <Lightbox
-            theme={{ container: { background: 'rgba(0, 0, 0, 0.85)' } }}
-            images={this.state.photos.map(x => ({ ...x, srcset: x.srcSet, caption: x.title }))}
-            backdropClosesModal={true}
-            onClose={this.closeLightbox}
-            onClickPrev={this.gotoPrevious}
-            onClickNext={this.gotoNext}
-            currentImage={this.state.currentImage}
-            isOpen={this.state.lightboxIsOpen}
-            width={1600}
-          />
-
-          {!this.state.loadedAll && (
-            <div className="loading-msg" id="msg-loading-more">
-              Loading
-            </div>
-          )}
         </div>
-      );
-    } else {
-      return (
-        <div className="App">
-          <div id="msg-app-loading" className="loading-msg">
-            Loading
-          </div>
-        </div>
-      );
-    }
+
+        <Lightbox
+          theme={{ container: { background: 'rgba(0, 0, 0, 0.85)' } }}
+          images={photos.map(x => ({ ...x, srcset: x.srcSet, caption: x.title }))}
+          backdropClosesModal={true}
+          onClose={this.closeLightbox}
+          onClickPrev={this.gotoPrevious}
+          onClickNext={this.gotoNext}
+          currentImage={currentImage}
+          isOpen={lightboxIsOpen}
+          width={1600}
+        />
+      </div>
+    );
   }
 }
 
 const EnhancedApp = withContentRect('bounds')(App);
 
-ReactDOM.render(<EnhancedApp />, document.getElementById('app'));
+ReactDOM.render(<EnhancedApp photos={photos} />, document.getElementById('app'));
