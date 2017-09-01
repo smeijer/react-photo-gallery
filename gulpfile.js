0159c98bfc6b9fb4b56c7999e69bed209a8599cd
Stephan Meijer <stephan@meijer.ws>
2017-09-01 17:12:42 +0200

    Improve build script, so other components are also expoded

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,27 @@
 var gulp = require('gulp'),
-    initGulpTasks = require('react-component-gulp-tasks');
+  initGulpTasks = require('react-component-gulp-tasks');
+
+var COMPONENT_NAME = 'Gallery';
+var pkg = JSON.parse(require('fs').readFileSync('./package.json'));
 
 var taskConfig = {
+  component: {
+    name: COMPONENT_NAME,
+    file: './index.js',
+    src: './src',
+    dist: './dist',
+    pkgName: pkg.name,
+    dependencies: Object.keys(pkg.dependencies),
+  },
 
-	component: {
-		name: 'Gallery',
-		dependencies:['react', 'react-dom', 'react-images']
-	},
-
-	example: {
-		src: 'examples/src',
-		dist: 'examples/dist',
-		standalone: true,
-		files: [
-			'.gitignore',
-			'.npmignore',
-			'favicon.ico',
-			'index.html'
-		],
-		scripts: [
-			'app.js'
-		],
-		less: [
-			'example.less'
-		]
-	}
-
+  example: {
+    src: 'examples/src',
+    dist: 'examples/dist',
+    standalone: true,
+    files: ['.gitignore', '.npmignore', 'favicon.ico', 'index.html'],
+    scripts: ['app.js'],
+    less: ['example.less'],
+  },
 };
 
 initGulpTasks(gulp, taskConfig);
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Gallery from './Gallery';
+import Photo from './Photo';
+
+export { Gallery, Photo };
+
+// backwards compatibility
+export default Gallery;
