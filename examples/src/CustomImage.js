fe0cfad634bcc20f2bfaf15c3aa856c9b2116133
Stephan Meijer <stephan@meijer.ws>
2017-09-01 17:56:49 +0200

    Fix and improve CustomImage example

--- examples/src/CustomImage.js
+++ examples/src/CustomImage.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import { Photo } from 'react-photo-gallery';
 
 const style = `
   .thumb {
@@ -6,27 +7,76 @@ const style = `
     position: relative;
   }
 
-  .thumb > .img {
-    position: absolute;
-    background-size: cover;
-    height: 100%;
+  .overlay {
     width: 100%;
-    transition: transform .5s;
-    transform: scale(1,1);
-    z-index: -1;
+    height: 100%;
+    opacity: 0;
+    transition: opacity .2s linear;
   }
-
-  .thumb:hover .img {
-    transform: scale(1.25,1.25);
+  
+  .thumb:hover .overlay {
+    opacity: 1;
+    background: linear-gradient(transparent 65%, rgba(0,0,0,.35));
   }
- `;
+  
+  .text {
+    font-size: 14px;
+    color: #fff;
+    font-weight: 600;
+    min-height: 17px;
+    
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 40px;
+    width: 100%;
+    box-sizing: border-box;
+    padding: 0 8px 4px;
+  }
+  
+  .title, .attr {
+    display: block;
+    margin: 2px 0;
+    text-shadow: 0 0 2px #000;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    width: 100%;
+    z-index: 2;
+  }
+  
+  .attr {
+    font-size: 11px;
+    color: rgba(255, 255, 255, 0.7);
+    transition: color .15s linear;
+  }
+  
+  .attr:hover {
+    color: rgba(255, 255, 255, 1);
+  }
+`;
 
-const CustomImage = ({ index, photo, photo: { height, width, src }, onClick }) => (
-  <div className="thumb" style={{ height, width }} onClick={() => onClick({ index, photo })}>
-    <div className="img" style={{ backgroundImage: `url(${src})` }} />
+const handleClick = (event, data) => {
+  console.log('click', data);
+};
 
-    <style>{style}</style>
-  </div>
-);
+class CustomImage extends PureComponent {
+  render() {
+    const { photo } = this.props;
+
+    return (
+      <Photo className="thumb" {...this.props} onClick={handleClick}>
+        <div className="overlay">
+          <div className="text">
+            <span className="title">{photo.title}</span>
+            <span className="attr">{photo.attr}</span>
+          </div>
+        </div>
+
+        <style>{style}</style>
+      </Photo>
+    );
+  }
+}
 
 export default CustomImage;
