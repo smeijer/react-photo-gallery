9aa413198cfc8a3e0b4169474ed85acc1e3446b7
Stephan Meijer <stephan@meijer.ws>
2017-09-01 11:17:10 +0200

    Mark components as being Pure

--- src/Photo.js
+++ src/Photo.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 const imgStyle = { verticalAlign: 'bottom' };
 const imgStyleWithOnClick = { ...imgStyle, cursor: 'pointer' };
 
-class Photo extends Component {
+class Photo extends PureComponent {
   constructor() {
     super();
     this.handleClick = this.handleClick.bind(this);
